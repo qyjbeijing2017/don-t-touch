7db9e992f5c768ab5918302593a73539b8e4e3d2
qyj <qyjbeijing@gmail.com>
2023-07-08 10:25:14 +0800

    MOD: 暂存

--- Assets/Scripts/BufferHandler.cs
+++ Assets/Scripts/BufferHandler.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class BufferHandler : MonoBehaviour
 {
+    public bool stagging = false;
+    public bool linking = false;
+    public int dot{
+        get{
+            return m_dot;
+        }
+    }
+
+
+
 
     // Start is called before the first frame update
     void Start()
